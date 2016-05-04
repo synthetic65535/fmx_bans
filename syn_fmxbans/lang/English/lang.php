9a0bca800306213d5238ce4b7bb53d6bfd4c380f
Artem Khakimov <artem@sablab.org>
2016-05-04 10:50:08 +0300

    First Commit

---
+++ syn_fmxbans/lang/English/lang.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * syn_fmxbans
+ *
+ * Copyright (c) 2016 Artyom Khakimov
+ *
+ * GPL License
+ *
+ **/
+
+if (!defined('FORUM')) die();
+
+
+$lang_syn_fmxbans = array(
+	'Hardware bans' => 'Hardware bans',
+	'New hardware of banned people' => 'New hardware of banned people',
+	'Synonims' => 'Synonims',
+	'Hardware' => 'Hardware',
+	'banned' => 'banned',
+	'unbanned' => 'unbanned',
+	'Commands' => 'Commands',
+	'Ban' => 'Ban',
+	'Unban' => 'Unban',
+	'Moders see multiaccounts' => 'Moders see multiaccs',
+	'Allow moders to see user multiaccounts' => 'Allow moders to see user multiaccounts'
+);
+
+?>
---
+++ syn_fmxbans/lang/Russian/lang.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * syn_fmxbans
+ *
+ * Copyright (c) 2016 Artyom Khakimov
+ *
+ * GPL License
+ *
+ **/
+
+if (!defined('FORUM')) die();
+
+
+$lang_syn_fmxbans = array(
+	'Hardware bans' => 'Баны по железу',
+	'New hardware of banned people' => 'Новое оборудование забаненных',
+	'Synonims' => 'Синонимы',
+	'Hardware' => 'Оборудование',
+	'banned' => 'заблокировано',
+	'unbanned' => 'разблокировано',
+	'Commands' => 'Команды',
+	'Ban' => 'Забанить',
+	'Unban' => 'Разбанить',
+	'Moders see multiaccounts' => 'Модеры видят мультиакки',
+	'Allow moders to see user multiaccounts' => 'Разрешить модераторам видеть синонимы аккаунтов пользователей'
+);
+
+?>
